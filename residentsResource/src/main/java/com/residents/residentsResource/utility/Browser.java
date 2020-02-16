6720cf7617d228656adac957c994fb88e6696449
KN RAI <KN RAI@LAPTOP-3QC3HN03>
2020-02-16 15:52:25 +0530

    2ND CHECK

--- residentsResource/src/main/java/com/residents/residentsResource/utility/Browser.java
+++ residentsResource/src/main/java/com/residents/residentsResource/utility/Browser.java
@@ -5,7 +5,7 @@ public class Browser {
 	public String setup() {
 		System.out.print("Hello Wrold");
 		System.out.print("Hello Wrold1");
-		
+		System.out.print("Hello Wrold1");		
 		return "Sample";
 	}
 }
