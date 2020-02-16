b0689669de469d5c902afcbacc48dcd1d31c4fd6
KN RAI <KN RAI@LAPTOP-3QC3HN03>
2020-02-16 12:29:36 +0530

    first commit into local repostory

---
+++ residentsResource/src/main/java/com/residents/residentsResource/utility/Browser.java
@@ -0,0 +1,9 @@
+package com.residents.residentsResource.utility;
+
+public class Browser {
+
+	public String setup() {
+		System.out.print("Hello Wrold");
+		return "Sample";
+	}
+}
---
+++ residentsResource/src/test/java/com/residents/residentsResource/test/utility/browser.java
@@ -0,0 +1,14 @@
+package com.residents.residentsResource.test.utility;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class browser {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
